7749c3ee17bfb3320a6bd7272e424f491f6b5d8f
Kaito Sugimura <sugimurakaito@gmail.com>
2023-07-19 02:45:47 -0600

    Add damage animations

--- src/Pages/Card.jsx
+++ src/Pages/Card.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
 import styles from "./Card.module.css";
 
-export default function Card({number}) {
+export default function Card({ number, bAttack }) {
   return (
-    <div className={styles.cardRoot}>
-      {number}
+    <div
+      className={styles.cardRoot}
+      style={{
+        width: bAttack ? "0vw" : "6.5vw",
+        height: bAttack ? "0vw" : "6.5vw",
+        border: bAttack? "none": "0.3vw solid rgba(43, 43, 43, 0.13)",
+        transition: bAttack ? "width 0.5s ease-out, height 0.5s ease-out, border 0.5s ease-out" : "none",
+      }}
+    >
+      {!bAttack && number}
     </div>
-  )
+  );
 }
--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -12,18 +12,21 @@ export default function Main() {
   };
   const [cards, setCards] = useState(getNewSplitShuffleArrays());
   const [tiltValues, setTiltValues] = useState({ x: 0, y: 0 });
-  const [combatState, setCombatState] = useState(0); // Switch to using Enum later
+  const [combatState, setCombatState] = useState([0, 0]); // Switch to using Enum later
+
+  const [enemyMaxHealth, setEnemyMaxHealth] = useState(500);
+  const [enemyCurrentHealth, setEnemyCurrentHealth] = useState(enemyMaxHealth);
 
   const handleMouseMove = (e) => {
     const { clientX, clientY } = e;
     const x = (clientX - window.innerWidth * 0.5) * 0.002;
-    const y = (clientY - window.innerHeight * 0.5) * 0.002;
+    const y = (clientY - window.innerHeight * 0.5) * -0.002;
 
     setTiltValues({ x, y }); // Not efficient at all, but it works for now...
   };
 
   const getActionComponent = () => {
-    switch (combatState) {
+    switch (combatState[0]) {
       case 0:
         return (
           <button
@@ -31,29 +34,44 @@ export default function Main() {
             onClick={(event) => {
               event.preventDefault();
               setCards(getNewSplitShuffleArrays());
-              setCombatState(1);
+              setCombatState([1, 0]);
             }}
           >
             Roll Again
           </button>
         );
       case 1:
+        const findSum = (i) => {
+          let sum = 0;
+          cards[i].forEach((card) => {
+            sum += card;
+          });
+          return sum;
+        };
         return (
           <div className={styles.bottomCenter}>
             <button
-              className={styles.selectionButton}
+              className={`${styles.selectionButton} ${styles.selectionButtonLeft}`}
               onClick={(event) => {
                 event.preventDefault();
-                setCombatState(0);
+                setCombatState([2, 1]);
+                setTimeout(() => {
+                  setEnemyCurrentHealth((prev) => prev - findSum(0));
+                  setCombatState([0, 1]);
+                }, 500);
               }}
             >
               Left
             </button>
             <button
-              className={styles.selectionButton}
+              className={`${styles.selectionButton} ${styles.selectionButtonRight}`}
               onClick={(event) => {
                 event.preventDefault();
-                setCombatState(0);
+                setCombatState([3, 2]);
+                setTimeout(() => {
+                  setEnemyCurrentHealth((prev) => prev - findSum(1));
+                  setCombatState([0, 2]);
+                }, 500);
               }}
             >
               Right
@@ -66,7 +84,7 @@ export default function Main() {
   };
 
   return (
-    <div onMouseMove={handleMouseMove}>
+    <div onMouseMove={handleMouseMove} className={styles.mainRoot}>
       {/* Most likely better to put background in its own file if this was a full project */}
       <div className={styles.BackgroundPerspective}>
         <div
@@ -79,27 +97,81 @@ export default function Main() {
       </div>
       <img src={slime1} className={styles.enemyImage}></img>
 
-        <div>
-          <div className={styles.enemyHealthBar}></div>
-          <p className={styles.enemyName}></p>
+      <div className={styles.topBarContainer}>
+        <p className={styles.enemyName}>Normal Slime</p>
+        <div className={styles.enemyHealthBar}>
+          <div
+            className={styles.enemyHealthBarFill}
+            style={{
+              width: `${Math.max(
+                (enemyCurrentHealth / enemyMaxHealth) * 100,
+                0
+              )}%`,
+            }}
+          ></div>
         </div>
+      </div>
 
       <div className={styles.cardBox}>
-        <div className={styles.leftCardBox}>
-          {cards[0].map((card, index) => (
-            <Card key={index} number={card} />
-          ))}
+        <div
+          className={styles.leftBoxPos}
+          style={{
+            transform:
+              combatState[1] === 1
+                ? "translate(-8%, 0%)"
+                : "translate(-108%, 0%)",
+            top: combatState[1] === 1 ? "43%" : "65%",
+            transition:
+              combatState[1] === 1
+                ? "transform 0.5s ease-out, top 0.5s ease-in"
+                : "none",
+          }}
+        >
+          {combatState[1] === 1 ? (
+            <div className={styles.leftCardBoxNoAfter}>
+              {cards[0].map((card, index) => (
+                <Card key={index} number={card} bAttack={true} />
+              ))}
+            </div>
+          ) : (
+            <div className={styles.leftCardBox}>
+              {cards[0].map((card, index) => (
+                <Card key={index} number={card} bAttack={false} />
+              ))}
+            </div>
+          )}
         </div>
-        <div className={styles.rightCardBox}>
-          {cards[1].map((card, index) => (
-            <Card key={index} number={card} />
-          ))}
+        <div
+          className={styles.rightBoxPos}
+          style={{
+            transform:
+              combatState[1] === 2
+                ? "translate(-92%, 0%)"
+                : "translate(8%, 0%)",
+            top: combatState[1] === 2 ? "43%" : "65%",
+            transition:
+              combatState[1] === 2
+                ? "transform 0.5s ease-out, top 0.5s ease-in"
+                : "none",
+          }}
+        >
+          {combatState[1] === 2 ? (
+            <div className={styles.rightCardBoxNoAfter}>
+              {cards[1].map((card, index) => (
+                <Card key={index} number={card} bAttack={true} />
+              ))}
+            </div>
+          ) : (
+            <div className={styles.rightCardBox}>
+              {cards[1].map((card, index) => (
+                <Card key={index} number={card} bAttack={false} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
 
       {getActionComponent()}
-
-
     </div>
   );
 }
